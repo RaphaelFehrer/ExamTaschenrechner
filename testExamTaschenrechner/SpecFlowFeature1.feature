eacb25bc3e4c90cc6ede65231e3173a787a984ac
Raphael Fehrer <raphael@fehrer.at>
2020-11-05 10:28:20 +0100

    Projektdateien hinzufügen.

---
+++ testExamTaschenrechner/SpecFlowFeature1.feature
@@ -0,0 +1,22 @@
+﻿Feature: Taschenrechner
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the result of a equation
+
+@mytag
+Scenario: get sinus of a number
+	Given the number is 3.14159
+	When the equation is sinus
+	Then the result should be 0
+
+@mytag
+Scenario: get cosinus of a number
+	Given the number is 3.14159
+	When the equation is cosinus
+	Then the result should be -1
+
+@mytag
+Scenario: get tangens of a number
+	Given the number is 3.14159
+	When the equation is tangens
+	Then the result should be 0
---
+++ testExamTaschenrechner/TaschenrechnerSteps.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace testExamTaschenrechner
+{
+    [Binding]
+    public class TaschenrechnerSteps
+    {
+        double number, result;
+
+        [Given(@"the number is (.*)")]
+        public void GivenTheNumberIs(double p0)
+        {
+            number = p0;
+        }
+        
+        [When(@"the equation is sinus")]
+        public void WhenTheEquationIsSinus()
+        {
+            result = Math.Sin(number);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(double p0)
+        {
+            double expected = p0;
+            double actual = result;
+            Assert.Equal(expected, actual, 5);
+        }
+
+        [When(@"the equation is cosinus")]
+        public void WhenTheEquationIsCosinus()
+        {
+            result = Math.Cos(number);
+        }
+
+        [When(@"the equation is tangens")]
+        public void WhenTheEquationIsTangens()
+        {
+           result = Math.Tan(number);
+        }
+
+
+    }
+}
